72396d2e542cfc214c16204e6c1bedf8cd09fee2
shubham <legendshubham4@gmail.com>
2023-09-15 08:37:56 +0545

    javascript

---
+++ fegma project/index.js
@@ -0,0 +1,94 @@
+// alert("alert")
+
+console.log("hari")
+
+firstn="shyam"
+lastn="ram"
+ firstn="hello"
+console.log(firstn,lastn)
+console.log(firstn,lastn)
+console.log(firstn,lastn)
+console.log(firstn,lastn)
+console.log(firstn,lastn)
+console.log(firstn,lastn)
+
+
+age="20"
+console.log("my age is" ,age)
+
+
+
+
+
+fN="shubham"
+lN="gupta"
+ 
+full_name=fN+lN
+fullName=(fN,lN)
+console.log("my name is ",full_name)
+
+console.log("my name is",fullName)
+
+
+function print()
+{
+    document.getElementById().window.print();
+}
+
+let price ;
+price = 100000 ;
+console.log("I phone price is",price);
+
+const pi=3.14
+console.log("the value of pi is",pi)
+
+
+
+price=90000
+console.log(" new Iphone price is",price)
+
+function myFunction(){
+    document.getElementById("listing").innerHTML ="shubham";
+
+}
+
+
+
+// window.alert("is connected");
+
+
+let course = ["bca","bit","bba","mba"];
+console.log("course of array is " , course[2], course[0]);
+
+
+
+let person = ["ram","shyam","hari"];
+console.log("the list of name is",person);
+person[1]="shubham";
+console.log(person);
+
+
+let tv = {
+    tvname: "lg",
+    color:"white",
+    price:5000,
+
+
+}
+console.log("TV--",tv);
+
+
+let colorRed = {
+
+    hax : "#03215",
+    colorname :"red",
+    rgbcode:"255,0,0" 
+
+
+}
+console.log("color property is",colorRed);
+
+
+
+let allColor = [colorRed,tv];
+console.log(allColor);
